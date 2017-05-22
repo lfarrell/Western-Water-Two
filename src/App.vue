b35571220304838c16383e517de324f2b99a0b6e
lfarrell <farrelldlfarrell@gmail.com>
2017-05-22 18:53:46 -0500

    Add routes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,55 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" ng-init="isCollapsed=true" ng-click="isCollapsed = !isCollapsed">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#/">Western Water (2000-2017)</a>
+        </div>
+        <div class="navbar-collapse" ng-class="{collapse: isCollapsed}">
+          <ul class="nav navbar-nav">
+            <li class="dropdown" dropdown on-toggle="toggled(open)">
+              <a class="dropdown-toggle" dropdown-toggle href="#">Lower Colorado States<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#/arizona">Arizona</a></li>
+                <li><a href="#/california">California</a></li>
+                <li><a href="#/nevada">Nevada</a></li>
+              </ul>
+            </li>
+            <li class="dropdown" dropdown on-toggle="toggled(open)">
+              <a class="dropdown-toggle" dropdown-toggle href="#">Upper Colorado States<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#/colorado">Colorado</a></li>
+                <li><a href="#/new_mexico">New Mexico</a></li>
+                <li><a href="#/utah">Utah</a></li>
+                <li><a href="#/wyoming">Wyoming</a></li>
+              </ul>
+            </li>
+            <li class="dropdown" dropdown on-toggle="toggled(open)">
+              <a class="dropdown-toggle" dropdown-toggle href="#">Pacific Northwest States<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#/idaho">Idaho</a></li>
+                <li><a href="#/oregon">Oregon</a></li>
+                <li><a href="#/washington">Washington</a></li>
+              </ul>
+            </li>
+            <li><router-link to="/texas">Texas</router-link></li>
+            <li><a href="#/about">About</a></li>
+            <li><a href="http://www.deanfarr.com">Other Visuals</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
     <router-view></router-view>
+    <div class="col-offset-sm-1 col-sm-10" v-cloak>
+      <p>If you have any questions or comments feel free to contact me via Twitter at @farrelldlfarrel, or email
+        at farrelldlfarrell at gmail.com</p>
+    </div>
   </div>
 </template>
 
@@ -15,7 +63,6 @@ export default {
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   margin-top: 60px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import Index from '@/components/maps/index'
+import Map from '@/components/Map'
+import Arizona from '@/components/Arizona'
+import California from '@/components/California'
+import Colorado from '@/components/Colorado'
+import Idaho from '@/components/Idaho'
+import New_Mexico from '@/components/New_Mexico'
+import Nevada from '@/components/Nevada'
+import Oregon from '@/components/Oregon'
+import Texas from '@/components/Texas'
+import Utah from '@/components/Utah'
+import Washington from '@/components/Washington'
+import Wyoming from '@/components/Wyoming'
+import About from '@/components/About'
 
 
 Vue.use(Router);
@@ -16,8 +28,75 @@ export default new Router({
     {
       name: 'MainMap',
       path: '/',
-      folder: true,
-      component: Index
+      props: {
+        dataFile: 'all_resv.csv',
+        mapFile: 'contig_us.geo.json',
+        resFile: 'all.csv',
+        res: 'Shasta Dam',
+        hasKey: true
+      },
+      component: Map
     },
+    {
+      name: 'Arizona',
+      path: '/arizona',
+      component: Arizona
+    },
+    {
+      name: 'California',
+      path: '/california',
+      component: California
+    },
+    {
+      name: 'Colorado',
+      path: '/colorado',
+      component: Colorado
+    },
+    {
+      name: 'Idaho',
+      path: '/idaho',
+      component: Idaho
+    },
+    {
+      name: 'Nevada',
+      path: '/nevada',
+      component: Nevada
+    },
+    {
+      name: 'New Mexico',
+      path: '/new_mexico',
+      component: New_Mexico
+    },
+    {
+      name: 'Oregon',
+      path: '/oregon',
+      component: Oregon
+    },
+    {
+      name: 'Texas',
+      path: '/texas',
+      component: Texas
+    },
+    {
+      name: 'Utah',
+      path: '/utah',
+      component: Utah
+    },
+    {
+      name: 'Washington',
+      path: '/washington',
+      component: Washington
+    },
+    {
+      name: 'Wyoming',
+      path: '/wyoming',
+      component: Wyoming
+    },
+    {
+     path: '/about',
+     name: 'About',
+     component: About
+     }
+
   ]
 })
