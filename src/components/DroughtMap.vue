63eeb132574c2cd5edb74d86be956a1fc9706cba
lfarrell <farrelldlfarrell@gmail.com>
2017-06-23 20:47:29 -0400

    Load drought map files

--- src/components/DroughtMap.vue
+++ src/components/DroughtMap.vue
@@ -1,10 +1,13 @@
 <template>
-  <svg></svg>
+  <div class="col-sm-12 col-lg-12">
+    <svg class="is-map" id="drought_map" :height="graph_height" :width="graph_width"></svg>
+  </div>
 </template>
 
 <script>
   import * as d3 from 'd3';
   import {tip} from './utilities/tip';
+  import {formatting} from './utilities/formatting';
 
   export default {
     name: 'DroughtMap',
@@ -12,60 +15,51 @@
     data() {
       return {
         graph_height: 500 - this.margins().top -this. margins().bottom,
-        graph_width: 600 - this.margins().left - this.margins().right,
+        graph_width: window.innerWidth - this.margins().left - this.margins().right,
         graph_translate: `translate(${this.margins().left},${this.margins().top})`
       }
     },
 
     props: {
-      dataFile: String,
-      mapFile: String
+      mapFile: String,
+      centersFile: String,
+      dataFile: String
     },
 
     methods: {
+      margins() {
+        return {top: 25, right: 40, bottom: 0, left: 75};
+      },
+
       draw() {
         let vm = this;
         let tip_div = tip.tipDiv();
 
-        let svg = d3.select('#map');
-        let width = svg.attr('width');
-        let height = svg.attr('height');
-
-        let scale = 1,
-          projection = d3.geoAlbers()
-            .scale(scale)
-            .translate([0,0]);
+        let svg = d3.select('#drought_map');
 
         d3.queue()
           .defer(d3.json, `static/data/maps/${this.mapFile}`)
-         // .defer(d3.csv, `static/data/drought/${this.dataFile}`)
-          .await(function(error, map, stations, data) {
-            let path = d3.geoPath().projection(projection);
-            let bounds = path.bounds(map);
-            scale = .95 / Math.max((bounds[1][0] - bounds[0][0]) / width, (bounds[1][1] - bounds[0][1]) / height);
-
-            let state = vm.whichState;
-
-            let translation = [(width - scale * (bounds[1][0] + bounds[0][0])) / 2,
-              (height - scale * (bounds[1][1] + bounds[0][1])) / 2];
-
-            // update projection
-            projection = d3.geoAlbersUsa()
-              .scale(scale)
-              .translate(translation);
-            path = path.projection(projection);
-
+          .defer(d3.json, `static/data/drought/centers/${this.centersFile}`)
+          .defer(d3.csv, `static/data/drought/${this.dataFile}`)
+          .await(function(error, map, centers, data) {
+            let map_path = formatting.mapScaling(vm.graph_height, vm.graph_width, map);
+            let path = map_path.path;
             let maps = svg.selectAll('path')
-              .data(map.features).call(zoom);
+              .data(map.features);
 
             maps.enter()
               .append('path')
               .merge(maps)
+              .attr('vector-effect', 'non-scaling-stroke')
               .attr('d', path);
 
             maps.exit().remove();
           });
       }
+    },
+
+    mounted() {
+      this.draw();
     }
   }
 </script>
