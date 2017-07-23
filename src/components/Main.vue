4c3db3519ef9a055a61700097a4976e2e5e36469
lfarrell <farrelldlfarrell@gmail.com>
2017-07-22 20:23:23 -0400

    Add text for main page

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <p class="offset-sm-1 col-sm-10 offset-lg-1 col-lg-10 text-top">The western landscape is varied running from desert to rain forest.
+      However, most of the region tends towards aridness, requiring concentrated reservoirs and irrigation
+      to allow for large scale human use. The map and graph below shows the network of reservoirs across
+    the west. Much of the American West has been in and out of drought for most of the 21<sup>st</sup> century. Leading
+      to lower average water levels than would be typical. For an example, checkout reservoir levels in California
+      in 2015 and most of 2016.
+      Click a reservoir to display its levels. Hover over the graph to see the levels for a given month.</p>
     <main-map
       mapFile="western_us.json"
       dataFile="all_load.csv"
