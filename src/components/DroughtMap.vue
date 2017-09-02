03128fbe891ae1e0547127d116e131d104e5d937
lfarrell <farrelldlfarrell@gmail.com>
2017-09-01 20:27:24 -0400

    Update month

--- src/components/DroughtMap.vue
+++ src/components/DroughtMap.vue
@@ -66,7 +66,7 @@
         droughtType: '',
         droughtTypeLower: '',
         slider_data: {
-          value: '07/2017',
+          value: '08/2017',
           width: '80%',
           reverse: true,
           formatter: function(value) {
